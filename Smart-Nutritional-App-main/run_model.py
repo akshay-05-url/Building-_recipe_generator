17829edb44cf682561e039d37f64c219b3384a1e
Akshay <sripadaakshay15@gmail.com>
2025-06-03 19:04:50 +0530

    Delete Smart-Nutritional-App-main directory

--- Smart-Nutritional-App-main/run_model.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-# Load a local NLP pipeline
-nlp = pipeline("sentiment-analysis")  # Can also do summarization, question-answering, etc.
-
-# Test the model
-result = nlp("I love using Hugging Face models locally!")
-print(result)
-
